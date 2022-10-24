1dbe6cace6986446f6476eab1e98491ce309c598
Shokhruzmukhtarov <shoxruzchik880@gmail.com>
2022-10-24 14:24:33 +0500

    Shokhruz

--- lib/modeles/state.dart
+++ lib/modeles/state.dart
@@ -1,6 +1,10 @@
 enum MyState {
+  // ignore: constant_identifier_names
   ERROR,
+  // ignore: constant_identifier_names
   PURE,
+  // ignore: constant_identifier_names
   SUCCESS,
+  // ignore: constant_identifier_names
   LOADING,
 }
